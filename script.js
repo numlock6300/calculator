68383c10b1aac98068f8b51d9749aa2f6005d526
numlock6300 <numlock6300@gmail.com>
2022-04-27 11:44:45 +0300

    Created functions: add, subtract, multiply, divide , operate

---
+++ script.js
@@ -0,0 +1,31 @@
+function add(firstNumber, secondNumber){
+    return firstNumber + secondNumber;
+}
+
+function subtract(firstNumber, secondNumber){
+    return firstNumber - secondNumber;
+}
+
+function multiply(firstNumber, secondNumber){
+    return firstNumber * secondNumber;
+}
+
+function divide(firstNumber, secondNumber){
+    return firstNumber / secondNumber;
+}
+
+function operate(firstNumber, secondNumber, operator){
+    switch(operator){
+        case "+":
+            return add(firstNumber,secondNumber);
+        case "-":
+            return subtract(firstNumber, secondNumber);
+        case "*":
+            return multiply(firstNumber,secondNumber);
+        case "/":
+            return divide(firstNumber, secondNumber);
+        default:
+            console.log("Error.");
+    }
+}
+
